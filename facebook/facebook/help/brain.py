2898756fe066aadab108c684201478128d8e1686
lewington <lewington@student.unimelb.edu.au>
2018-10-05 09:00:40 +1000

    spider working

---
+++ facebook/facebook/help/brain.py
@@ -0,0 +1,37 @@
+small_step = 500000
+
+class brain():
+    def __init__(self):
+        self.recent_identifiers = []
+        self.duplicate_count = 0
+        self.current_step = small_step
+        self.duplicates_this_step = 1
+
+    def is_duplicate(self, comment: dict) -> bool:
+        identifier = comment["name"] + comment["timestamp"] + comment["link"]
+        if identifier in self.recent_identifiers:
+            self.duplicate_count += 1
+            self.duplicates_this_step += 1
+            print(self.duplicate_count)
+            return True
+        
+        self.update_duplicates(identifier)
+        return False
+    
+    def update_duplicates(self, new_identifier: str):
+        if len(self.recent_identifiers) > 100:
+            self.recent_identifiers.pop(0)
+
+        self.recent_identifiers.append(new_identifier)
+    
+    def calculate_next_step(self):
+        if self.duplicates_this_step > 1:
+            self.current_step += self.current_step / 5
+        elif self.duplicates_this_step < 1:
+            self.current_step -= self.current_step / 4
+    
+    def step(self) -> int:
+        self.calculate_next_step()
+        self.duplicates_this_step = 0
+        print("=---------------------------{}".format(self.current_step))
+        return self.current_step
--- facebook/facebook/spiders/comments_spider.py
+++ facebook/facebook/spiders/comments_spider.py
@@ -1,15 +1,17 @@
 import scrapy
-from facebook.help import sel, conv
+from facebook.help import sel, conv, brain
 from facebook import db
 
 max_cn = 222645646350
 small_step = 500000
 step_size = 1500000
 dub_step = 3000000
+huge_step = 10000000
 
-bunnings = "https://www.facebook.com/pages_reaction_units/more/?page_id=539309746236829&cursor=%7B%22timeline_cursor%22%3A%{cn}%22%2C%22timeline_section_cursor%22%3Anull%2C%22has_next_page%22%3Afalse%7D&surface=www_pages_community_tab&unit_count=8&dpr=1&fb_dtsg_ag&__user=0&__a=1&__req=4&__be=-1&__pc=PHASED%3ADEFAULT&__rev=4376521"
 
+energy_oz = "https://www.facebook.com/pages_reaction_units/more/?page_id=539309746236829&cursor=%7B%22timeline_cursor%22%3A%{cn}%22%2C%22timeline_section_cursor%22%3Anull%2C%22has_next_page%22%3Afalse%7D&surface=www_pages_community_tab&unit_count=8&dpr=1&fb_dtsg_ag&__user=0&__a=1&__req=4&__be=-1&__pc=PHASED%3ADEFAULT&__rev=4376521"
 
+bunnings = "https://www.facebook.com/pages_reaction_units/more/?page_id=1657775594549456&cursor=%7B%22timeline_cursor%22%3A%{cn}%22%2C%22timeline_section_cursor%22%3Anull%2C%22has_next_page%22%3Afalse%7D&surface=www_pages_community_tab&unit_count=8&dpr=1&fb_dtsg_ag&__user=0&__a=1&__req=4&__be=-1&__pc=PHASED%3ADEFAULT&__rev=4376521"
 
 class QuotesSpider(scrapy.Spider):
     name = "comments"
@@ -17,9 +19,7 @@ class QuotesSpider(scrapy.Spider):
     def __init__(self):
         self.db = db.db()
         self.prev_highest_cn = self.db.get_highest_cn()
-        self.recent_identifiers = []
-        self.duplicate_count = 0
-
+        self.brain = brain.brain()
 
     def start_requests(self):
         # Find a good starting comment number
@@ -29,20 +29,20 @@ class QuotesSpider(scrapy.Spider):
         )
 
     def parse(self, response):
-        if QuotesSpider.has_correct_content_type:
-            self.current_cn -= small_step
+        if QuotesSpider.has_correct_content_type(response):
+            for comment in sel.all_comments(conv.body_html(response.body)):
+                comment_data = sel.comment_data(comment)
+                if not self.brain.is_duplicate(comment_data):
+                    yield comment_data
+
+            self.current_cn -= self.brain.step()
             yield scrapy.Request(
                 url=bunnings.format(cn=self.current_cn),
                 callback=self.parse
             )
-            for comment in sel.all_comments(conv.body_html(response.body)):
-                comment_data = sel.comment_data(comment)
-                if not self.is_duplicate(comment_data):
-                    yield comment_data
 
     def find_starting_cn(self, response):
-        print("------------------------------------------")
-        if QuotesSpider.has_correct_content_type:
+        if QuotesSpider.has_correct_content_type(response):
             self.starting_cn = self.prev_highest_cn
             self.higher_cn = self.starting_cn + dub_step
             self.first_user = sel.first_user(conv.body_html(response.body))
@@ -53,7 +53,7 @@ class QuotesSpider(scrapy.Spider):
 
     def check_higher_cn(self, response):
         # keep stepping upwards until the first name remains the same (at which point we've reached the cn for the first comment 
-        if QuotesSpider.has_correct_content_type:
+        if QuotesSpider.has_correct_content_type(response):
             new_first_user = sel.first_user(conv.body_html(response.body))
             if new_first_user != self.first_user:
                 self.first_user = new_first_user
@@ -75,22 +75,6 @@ class QuotesSpider(scrapy.Spider):
                     callback=self.parse
                 )
 
-    def is_duplicate(self, comment: dict) -> bool:
-        identifier = comment["name"] + comment["timestamp"] + comment["link"]
-        if identifier in self.recent_identifiers:
-            self.duplicate_count += 1
-            print(self.duplicate_count)
-            return True
-        
-        self.update_duplicates(identifier)
-        return False
-    
-    def update_duplicates(self, new_identifier: str):
-        if len(self.recent_identifiers) > 100:
-            self.recent_identifiers.pop(0)
-
-        self.recent_identifiers.append(new_identifier)
-
     @staticmethod
     def has_correct_content_type(response) -> bool:
         content_type = conv.to_str(response.headers[b"content-type"])
@@ -100,11 +84,6 @@ class QuotesSpider(scrapy.Spider):
                 return False
 
             print("We got an unexpected response, aborting scrape")
-            return False
-
-        try:
-            conv.to_json(response.body)
-        except:
             return False
 
         return True
