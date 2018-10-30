7649c808164f978b147a4410795eadf374e3d3dc
lewington <lewington@student.unimelb.edu.au>
2018-10-30 17:17:42 +1100

    Using global modules over local ones

--- facebook/scrape_likes.py
+++ facebook/scrape_likes.py
@@ -10,8 +10,8 @@ page_id = sys.argv[2]
 if page_id is None or page_name is None:
     raise ValueError("No page id or page name provided. Sort yourself out m8.")
 
-user_email = "hontyfonty@yandex.com"
-user_password = "HY67ccc881"
+user_email = "sallymuntzy@outlook.com"
+user_password = "r34hge%V&BF3ghf"
 
 likers_url = "https://m.facebook.com/search/{}/likers".format(page_id)
 
--- facebook/scrape_likes_moz.py
+++ facebook/scrape_likes_moz.py
@@ -3,7 +3,7 @@ import time
 import random
 from likers import steps
 from likers import save
-from likers import setup
+from lep.selenium import setup
 from facebook import db
 
 page_name = sys.argv[1]
@@ -12,10 +12,10 @@ page_id = sys.argv[2]
 if page_id is None or page_name is None:
     raise ValueError("No page id or page name provided. Sort yourself out m8.")
 
-user_email = "hontyfonty@yandex.com"
-user_password = "HY67ccc881"
+user_email = "sallymuntzy@outlook.com"
+user_password = "r34hge%V&BF3ghf"
 
-likers_url = "https://m.facebook.com/search/260823190756616/likers".format(page_id)
+likers_url = "https://m.facebook.com/search/{}/likers".format(page_id)
 
 driver = setup.driver_moz()
 
@@ -47,6 +47,4 @@ while True:
         break
 
     time.sleep(1)
-
-print("Hell yeah")
 
